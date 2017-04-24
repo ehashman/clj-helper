35d495b0f38a872f513bf8dfb6d120e0462b4527
Elana Hashman <ehashman@csclub.uwaterloo.ca>
2017-04-23 22:13:04 -0400

    Release version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-helper "0.1.0-SNAPSHOT"
+(defproject clj-helper "0.1.0"
   :description "Helps you package Clojure projects for Debian"
   :url "https://github.com/ehashman/clj-helper"
   :license {:name "Eclipse Public License"
