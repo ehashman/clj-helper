375676074806a195e24918cfc859209c2b80c6b1
Elana Hashman <ehashman@csclub.uwaterloo.ca>
2017-05-14 18:38:14 -0400

    Release version 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.2.0
+
+- Improve output packaging, fixing various typos.
+- Improve tests.
+
+# 0.1.0
+
+- Initial release.
+- Helps users package Clojure projects with Debian Maven Helper.
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-helper "0.1.1-SNAPSHOT"
+(defproject clj-helper "0.2.0"
   :description "Helps you package Clojure projects for Debian"
   :url "https://github.com/ehashman/clj-helper"
   :license {:name "Eclipse Public License"
